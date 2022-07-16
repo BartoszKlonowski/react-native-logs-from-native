7be40bb70263ed5fc059dcba45115abf8c3a8e50
BartoszKlonowski <Bartosz.Klonowski@callstack.com>
2022-07-16 23:40:56 +0200

    Install Jest and it's configurationa and tools

---
+++ package/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  extends: ["@callstack"],
+  env: {
+    "react-native/react-native": true,
+  },
+  parser: "babel-eslint",
+  plugins: ["react", "react-native"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      parser: "@typescript-eslint/parser",
+      plugins: ["@typescript-eslint"],
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+
+      parserOptions: {
+        project: ["./tsconfig.json"],
+      },
+    },
+  ],
+};
