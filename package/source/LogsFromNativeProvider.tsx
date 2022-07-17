9f2b737722e2dd8f1b7e67534f5ba3e533c624ea
BartoszKlonowski <Bartosz.Klonowski@callstack.com>
2022-07-17 23:49:09 +0200

    Implement the LogsFromNativeProvider and export it from index

--- package/source/LogsFromNativeProvider.tsx
+++ package/source/LogsFromNativeProvider.tsx
@@ -1,8 +1,35 @@
-import { VFC } from "react";
+import { useEffect, VFC } from "react";
+
+import {
+  NativeModules,
+  NativeEventEmitter,
+  DeviceEventEmitter,
+  Platform,
+} from "react-native";
+
+const { NativeLogger } = NativeModules;
 
 type Props = unknown;
 
 const LogsFromNativeProvider: VFC<Props> = () => {
+  useEffect(() => {
+    return () => {
+      subscription.remove();
+    }
+  }, []);
+
+  const notificationEventManager = Platform.select({
+    android: DeviceEventEmitter,
+    ios: new NativeEventEmitter(NativeLogger),
+  });
+
+  const subscription = notificationEventManager.addListener(
+    "newLogPrinted",
+    (payload) => {
+      console.log(payload.message);
+    }
+  );
+
   return null;
 };
 
