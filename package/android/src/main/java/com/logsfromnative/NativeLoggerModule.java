829184eefe8ef48a6d3d3ac77b32120381d297e1
BartoszKlonowski <Bartosz.Klonowski@callstack.com>
2022-07-17 23:43:46 +0200

    Implement the logs from native sending mechanism

--- package/android/src/main/java/com/logsfromnative/NativeLoggerModule.java
+++ package/android/src/main/java/com/logsfromnative/NativeLoggerModule.java
@@ -1,17 +1,22 @@
 package com.logsfromnative;
 
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
-import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+
+import java.util.ArrayList;
 
 public class NativeLoggerModule extends ReactContextBaseJavaModule {
 
     private final ReactApplicationContext reactContext;
+    private static ReactApplicationContext commonReactContext;
 
     public NativeLoggerModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
+        commonReactContext = reactContext;
     }
 
     @Override
@@ -19,9 +24,12 @@ public class NativeLoggerModule extends ReactContextBaseJavaModule {
         return "NativeLogger";
     }
 
-    @ReactMethod
-    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
-        // TODO: Implement some actually useful functionality
-        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+    static public void log(String message) {
+        WritableMap params = Arguments.createMap();
+        params.putString("message", message);
+
+        commonReactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("newLogPrinted", params);
     }
+
+    static private ArrayList<Log> logs = new ArrayList<>();
 }
